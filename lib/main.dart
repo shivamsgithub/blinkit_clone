6fe6716abca3727e2916f294b9c84b9c339eaa81
ShivamGit <shivampaliwal007@gmail.com>
2025-03-22 18:29:08 +0530

    first commit splash screen to login

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Blinkit',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: false,
       ),
--- lib/repository/screens/splash/splashscreen.dart
+++ lib/repository/screens/splash/splashscreen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:blinkit_clone/domain/constants/appcolors.dart';
+import 'package:blinkit_clone/repository/screens/splash/login/loginscreen.dart';
 import 'package:blinkit_clone/repository/widgets/UiHelper.dart';
 import 'package:flutter/material.dart';
 
@@ -8,6 +11,14 @@ class SplashScreen extends StatefulWidget{
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), (){
+      Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()));
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +28,6 @@ class _SplashScreenState extends State<SplashScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             UiHelper.Customimage(img: "blinkit_logo.png"),
-
           ],
         ),
       ),
